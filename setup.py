c3b4e60f9c56fa4516698fb77a1631f85f1c7084
TakesxiSximada <takesxi.sximada@gmail.com>
2014-12-05 22:08:01 +0900

    updatea version numbers 0.3.0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ DESCRIPTION = 'Sqlalchemy utilities'
 
 setup(
     name='azoth',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/TakesxiSximada/azoth',
     download_url='https://github.com/TakesxiSximada/azoth',
     license='BSD',
